2fbfeffadb7f9514c8fc744871730cfa799ed5a8
Kespion <kesputecryo@gmail.com>
2025-01-13 10:30:22 +0100

    First commit

---
+++ src/main/java/com/ynov/m2/advanced_software_development/cautious_guacamole/CautiousGuacamoleApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.m2.advanced_software_development.cautious_guacamole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CautiousGuacamoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CautiousGuacamoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynov/m2/advanced_software_development/cautious_guacamole/CautiousGuacamoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.m2.advanced_software_development.cautious_guacamole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CautiousGuacamoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
