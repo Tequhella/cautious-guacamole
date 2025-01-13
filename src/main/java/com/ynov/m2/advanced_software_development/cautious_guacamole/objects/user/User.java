baf494476c3708d43531f051449032414d31e060
Kespion <kesputecryo@gmail.com>
2025-01-13 11:40:54 +0100

    User

---
+++ src/main/java/com/ynov/m2/advanced_software_development/cautious_guacamole/objects/user/User.java
@@ -0,0 +1,58 @@
+package com.ynov.m2.advanced_software_development.cautious_guacamole.objects.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private boolean isAdmin;
+
+    public User(String name, String email, boolean isAdmin) {
+        this.name = name;
+        this.email = email;
+        this.isAdmin = isAdmin;
+    }
+
+    public User() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+}
